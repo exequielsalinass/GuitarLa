e149373cca7dd740577862008140feecd9ccb537
exequielsalinass <e.x.e_salinas3@outlook.com>
2024-03-21 16:55:51 -0300

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import Footer from './components/Footer'
-import Guitar from './components/Guitar'
-import Header from './components/Header'
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setData] = useState(db);
+
   return (
     <>
-      <Header/>
-      <Guitar/>
-      <Footer/>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+          <div className="row mt-5">
+            {data.map(() => {
+              
+            })}
+          </div>
+          <Guitar />
+      </main>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
