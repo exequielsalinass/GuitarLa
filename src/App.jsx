2507e4c7329dc30504515aa9e9eeb95df1ab62cd
exequielsalinass <e.x.e_salinas3@outlook.com>
2024-03-25 16:29:54 -0300

    validar carro vacio

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,9 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header 
+        carro={carro}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-function Header() {
+function Header({ carro }) {
   return (
     <header className="py-5 header">
       <div className="container-xl">
@@ -21,46 +21,50 @@ function Header() {
               />
 
               <div id="carrito" className="bg-white p-3">
-                <p className="text-center">El carrito esta vacio</p>
-                <table className="w-100 table">
-                  <thead>
-                    <tr>
-                      <th>Imagen</th>
-                      <th>Nombre</th>
-                      <th>Precio</th>
-                      <th>Cantidad</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <img
-                          className="img-fluid"
-                          src="/img/guitarra_02.jpg"
-                          alt="imagen guitarra"
-                        />
-                      </td>
-                      <td>SRV</td>
-                      <td className="fw-bold">$299</td>
-                      <td className="flex align-items-start gap-4">
-                        <button type="button" className="btn btn-dark">
-                          -
-                        </button>
-                        1
-                        <button type="button" className="btn btn-dark">
-                          +
-                        </button>
-                      </td>
-                      <td>
-                        <button className="btn btn-danger" type="button">
-                          X
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-
+                {carro.lenth === 0 ? (
+                  <p className="text-center">El carrito esta vacio</p>
+                ) : (
+                  <table className="w-100 table">
+                    <thead>
+                      <tr>
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {carro.map((guitar) => (
+                        <tr>
+                          <td>
+                            <img
+                              className="img-fluid"
+                              src={`/img/${guitar.image}.jpg`}
+                              alt="imagen guitarra"
+                            />
+                          </td>
+                          <td>{guitar.name}</td>
+                          <td className="fw-bold">${guitar.price}</td>
+                          <td className="flex align-items-start gap-4">
+                            <button type="button" className="btn btn-dark">
+                              -
+                            </button>
+                            {guitar.cantidad}
+                            <button type="button" className="btn btn-dark">
+                              +
+                            </button>
+                          </td>
+                          <td>
+                            <button className="btn btn-danger" type="button">
+                              X
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                )}
                 <p className="text-end">
                   Total pagar: <span className="fw-bold">$899</span>
                 </p>
